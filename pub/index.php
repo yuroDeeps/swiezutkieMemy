30dabb3a54bb5c2fca990d48285a62da98a0aef7
Yuro <nikodemnovtzki@gmail.com>
2023-02-08 10:55:17 +0100

    idk

--- pub/index.php
+++ pub/index.php
@@ -7,17 +7,26 @@
     <title>Document</title>
 </head>
 <body>
-    <form actions="" method="post">
+    <form actions="" method="post" enctype="multipart/form-data">
         <label for="uploadedFileInput">
                 Wybierz plik do wgrania na serwer:
         </label>
         <input type="file" name="uploadedFile" id="uploadedFileInput">
-        <input type="submit" value="Wyślij plik">
+        <input type="submit" value="Wyślij plik" name="submit">
     </form>
 
 <?php 
     if(isset($_POST['submit']))
-    var_dump($_FILES);
+    {
+        $targetDir = "img/";
+        $targetFile = $targetDir . $_FILES['uploadedFile']['name'];
+        if(file_exists($targetFile))
+        {
+            die("Plik o tej nazwie już istnieje");
+        }
+        move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $targetFile);
+        // var_dump($_FILES);
+    }
 ?>
 </body>
 </html>
