e4905b440c5e2e377563cbbc7272f240a7fca0ed
Yuro <nikodemnovtzki@gmail.com>
2023-03-01 10:39:33 +0100

    copy sandbox

--- src/Post.class.php
+++ src/Post.class.php
@@ -1,7 +1,32 @@
 <?php
 
 class Post {
-    
+    static function upload(string $tempFileName) {
+        $uploadDir = "img/";
+
+        $imageInfo = getimagesize($tempFileName);
+
+        if(!is_array($imageInfo)) {
+            die("Nieprawidłowy format obrazu!");
+        }
+
+        $randomSeed = rand(10000, 99999) . hrtime(true);
+
+        $hash = hash("sha256", $randomSeed);
+
+        $targetFileName = $uploadDir . $hash . ".webp";
+
+        if(file_exists($targetFileName))
+        {
+            die("Plik o tej nazwie już istnieje");
+        }
+
+        $imgString = file_get_contents($tempFileName);
+
+        $gdImage = @imagecreatefromstring($imgString);
+
+        imagewebp($gdImage, $targetFileName);
+    }
 }
 
 ?>
